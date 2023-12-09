94921d07e6cea42e81b809976a9bc78f810b2b28
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-09 21:31:22 +0500

    created two basic projects one is using for Utilty create-react-app, another is using create vite@latest

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Muhammad Umair Abbasi | Start React Series with create react app </h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1>Muhammad Umair Abbasi | start react serires with vite</h1>
+  )
+}
+
+export default App
