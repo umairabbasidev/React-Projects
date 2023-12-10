59bb1cc60230dea49ac4b798152c9f7f8b83fd36
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-10 12:16:09 +0500

    Understand the react flow and structure in class three

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,15 @@
 
 
+
+import Chai from "./chai"
 function App() {
 
 
   return (
+    <>
     <h1>Muhammad Umair Abbasi | start react serires with vite</h1>
+    <Chai/>
+    </>
   )
 }
 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+ return(
+  <h3>This is test heading </h3>
+ )
+}
+
+export default Chai
