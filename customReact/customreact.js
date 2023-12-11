3243dbde5b35f93b3b9f2c325d7bccd3dee36cf9
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-11 10:22:15 +0500

    creating custom react and understanding the flow of react and react work

---
+++ customReact/customreact.js
@@ -0,0 +1,33 @@
+function customRender(reactElement, container) {
+ /*
+  const domElement = document.createElement(reactElement.type);
+  console.log(domElement);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+  */
+ // version 2
+ const domElement = document.createElement(reactElement.type)
+ console.log(domElement)
+ domElement.innerHTML = reactElement.children
+ for(let prop in reactElement.props){
+  domElement.setAttribute(prop,reactElement.props[prop])
+  console.log(prop)
+ }
+ container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+console.log(mainContainer);
+
+customRender(reactElement, mainContainer);
