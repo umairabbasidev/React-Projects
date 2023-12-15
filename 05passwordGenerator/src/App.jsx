6b09026dca924504f1de022f96592a596d406b93
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-15 10:14:23 +0500

    task 2 done

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -24,7 +24,9 @@ function App() {
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
-       
+
+        <div className="flex shadow  rounded-lg overflow-hidden mb-4">inner</div>
+       outer
       </div>
     </>
   );
