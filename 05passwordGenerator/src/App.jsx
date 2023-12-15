304914054077a42760e356064d85ae075c7d9eb5
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-15 10:10:42 +0500

    task 1 done

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -8,31 +8,24 @@ function App() {
   const [password, setPassword] = useState("");
 
   const passwordGenerator = useCallback(() => {
-let pass = "";
-let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-if (nuberAllowed) str += "0123456789";
-if(characterAllowed) str+= "@#$%&*";
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (nuberAllowed) str += "0123456789";
+    if (characterAllowed) str += "@#$%&*";
 
+    for (let i = 1; i < str.length; i++) {
+      let char = Math.floor(Math.random().str.length + 1);
 
-for (let i = 1; i < str.length; i++) {
-let char = Math.floor(Math.random().str.length + 1)
-  
-}
-pass = str.charAt(char)
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, nuberAllowed, characterAllowed, setPassword]);
 
-  }, [
-    length,
-    nuberAllowed,
-    characterAllowed,
-    setPassword,
-  ]);
-
-  setPassword(pass)
   return (
     <>
-      <h1 className="text-4xl text-center text-white py-2">
-        Password Generator
-      </h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+       
+      </div>
     </>
   );
 }
