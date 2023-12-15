369896ca312d675da75e81714e4df34e44110b36
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-15 10:53:15 +0500

    task input one  done

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -24,18 +24,21 @@ function App() {
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-3 text-orange-500 bg-gray-700">
-        <h1 className="text-white text-center text-2xl ">Passord Generator</h1>
+        <h1 className="text-white text-center text-2xl my-3">
+          Passord Generator
+        </h1>
         <div className="flex shadow  rounded-lg overflow-hidden mb-4">
-       
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3 my-1"
+            className="outline-none w-full py-1 px-3 "
             placeholder="Password"
             readOnly
           />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
         </div>
-    
       </div>
     </>
   );
