5f5d39cb4e8598e76143193b487f6aa5406276ea
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-16 09:56:22 +0500

    task 1 completed created custom hook the  project currency convertor

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-   <h1 className='text-red-500'>Umair Abbasi</h1>
-  )
+  return <h1 className="text-red-500">Umair Abbasi</h1>;
 }
 
-export default App
+export default App;
