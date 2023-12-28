a659be759ab878dc2b672ec828c92e5f3a3e6551
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-28 15:19:31 +0500

    added 2 components one is header and othre is footer

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-<>
-<h1>Umair Abbasi</h1>
-</>
-  )
+    <>
+      <h1 className="text-red-500 bg-green-300 p-4">Umair Abbasi</h1>
+     
+    </>
+  );
 }
 
-export default App
+export default App;
